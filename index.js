bd56c63f71c6549c2d16223e54182d4ce750edc5
Ivan Rios <ivanrios.a@gmail.com>
2018-11-04 10:52:20 -0500

    Se guarda el suscriptor

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
-exports.hola = function (req, res) {
-    console.log(req.body, req.params, req.query, res.headers)
-    res.send('¡Hola mundo!');
-}
+'use strict';
 
-exports.adios = function (req, res) {
-    console.log(req.body, req.params, req.query, res.headers)
-    res.send('Hasta la vista!');
+exports.suscriptor = function (req, res) {
+
+    const email = req.body.email || req.query.email;
+    if (email){
+        const Datastore = require('@google-cloud/datastore');
+
+        const projectId = 'devfestcancun'
+        const datastore = new Datastore({
+            projectId: projectId,
+            keyFilename: './keyfile.json'
+        });
+
+        const kind = 'suscriptor';
+        const name = email;
+        const key = datastore.key([kind, name]);
+
+        const suscriptor = {
+            key: key,
+            data: [{
+                    name: 'email',
+                    value : email
+                },
+                {
+                    name :'fecha',
+                    value : new Date().toJSON()
+                }
+            ],
+        };
+
+        datastore
+            .save(suscriptor)
+            .then(() => {
+                console.log(`Saved ${suscriptor.key.name}: ${suscriptor.data.email}`);
+                res.send(`Saved ${suscriptor.key.name}: ${suscriptor.data.email}`)
+            })
+            .catch(err => {
+                console.error('ERROR:', err);
+            });
+    }
+    else{
+        res.send(`Datos incompletos`)
+    }
 }
 
