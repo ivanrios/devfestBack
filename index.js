a1fa7392e63531127e7518c46ecfe2a6fb559141
Ivan Rios <ivanrios.a@gmail.com>
2018-11-07 23:46:03 -0500

    Se agregan los cors

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 
 exports.correo = function (req, res) {
 
+    res.header('Content-Type', 'application/json');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    //respond to CORS preflight requests
+    if (req.method == 'OPTIONS') {
+        res.status(204).send('');
+    }
+    
+    
     const correo = req.body.email || req.query.email;
     if (correo){
         const enviaCorreo = require('./enviaCorreo');
